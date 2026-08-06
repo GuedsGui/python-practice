c3b5a7e30f6cfc1d064c88d176b156d27ddfab19
Guilherme Guedes <103967577+GuedsGui@users.noreply.github.com>
2026-08-06 08:26:55 -0300

    Create sum.py
    Código que calcula e imprime a soma entre dois números

---
+++ day003_print_sum/sum.py
@@ -0,0 +1,8 @@
+print("IMPRIMINDO A SOMA DE DOIS NÚMEROS")
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+soma = num1 + num2
+
+print(f"Resultado da soma: {num1} + {num2} = {soma}")
