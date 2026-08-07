1882ee048fda6a88b639fceb6c71d9e701975fd2
Guilherme Guedes <103967577+GuedsGui@users.noreply.github.com>
2026-08-07 07:59:36 -0300

    Create grades.py
    Código que pede as 4 notas de um aluno e calcula a média

---
+++ day004_grades/grades.py
@@ -0,0 +1,10 @@
+print("MÉDIA DO ALUNO")
+
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a segunda nota: "))
+nota3 = int(input("Digite a terceira nota: "))
+nota4 = int(input("Digite a quarta nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f"Primeira nota: {nota1} \nSegunda nota: {nota2} \nTerceira nota: {nota3} \nQuarta nota: {nota4} \nMédia: {media}")
