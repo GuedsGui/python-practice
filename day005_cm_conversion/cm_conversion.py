c89b92141f57917ffffa781605e24b07e274d9dd
Guilherme Guedes <103967577+GuedsGui@users.noreply.github.com>
2026-08-08 12:10:14 -0300

    Create cm_conversion.py
    Código que converte metros em centímetros

---
+++ day005_cm_conversion/cm_conversion.py
@@ -0,0 +1,7 @@
+print("CONVERSÃO DE METROS-CENTÍMETROS")
+
+print("Digite a quantidade de metros que quer converter para centímetros.")
+metros = float(input("Metros: "))
+centimetros = metros * 100
+
+print(f"{metros} metros contém {centimetros} centímetros.")
