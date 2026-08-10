5237ee7eb08ebbd6958c967882003443c9bfb89f
Guilherme Guedes <103967577+GuedsGui@users.noreply.github.com>
2026-08-10 08:21:07 -0300

    Create square.py
    Código que calcula o dobro da área do trabalho

---
+++ day007_square/square.py
@@ -0,0 +1,6 @@
+print("CÁLCULO DO DOBRO DA ÁREA DO QUADRADO")
+
+lado = float(input("Digite o lado do quadrado: "))
+area = lado ** 2
+dobro = area * 2
+print(f"O lado possui {lado}cm, a área possui {area}cm2 e seu dobro é {dobro}cm2.")
