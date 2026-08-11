8091694c10eddaaca2280967ebb6d08d987ac570
Guilherme Guedes <103967577+GuedsGui@users.noreply.github.com>
2026-08-11 08:35:22 -0300

    Create wage_calc.py
    Código que calcula o salário mensal

---
+++ day008_wage_calc/wage_calc.py
@@ -0,0 +1,8 @@
+print("CALCULANDO O SALÁRIO")
+
+valor_hora = float(input("Valor recebido por hora: "))
+total_hora = float(input("Horas trabalhadas no mês: "))
+
+salario = valor_hora * total_hora
+
+print(f"Seu salário mensal é R$ {salario}")
