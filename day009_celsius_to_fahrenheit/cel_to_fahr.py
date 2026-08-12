06790c90b20e25a63e355be32dd6a244189e2fc4
Guilherme Guedes <103967577+GuedsGui@users.noreply.github.com>
2026-08-12 08:35:28 -0300

    Create cel_to_fahr.py
    Código que converte a temperatura de fahrenheit em celsius

---
+++ day009_celsius_to_fahrenheit/cel_to_fahr.py
@@ -0,0 +1,6 @@
+print("Calculando Fahrenheit em Celsius")
+
+fahrenheit = float(input("Temperatura em Fahrenheit: "))
+celsius = 5 * ((fahrenheit-32) / 9)
+
+print(f"Temperatura em Celsius: {celsius:.2f}C°")
